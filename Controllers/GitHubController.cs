97b1c27a5024d349ab0149a3211d88d3f648208c
Colin Kern <Colin.Kern@trustage.com>
2024-05-07 12:46:42 -0500

    Github controller and page

---
+++ Controllers/GitHubController.cs
@@ -0,0 +1,27 @@
+﻿using CK_Website_2024.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CK_Website_2024.Controllers
+{
+    public class GitHubController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public GitHubController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
